05a69e1a3bcd1a15412bd6752fd9e71cd7d17c6f
Karttune <karttunenkonsta@gmail.com>
2024-07-03 14:50:24 +0300

    New XY and boxplot figures, IGV session

--- R/style.R
+++ R/style.R
@@ -16,6 +16,12 @@ theme_set(theme_bw() +
               legend.background = element_rect(color = "black", fill = NA, linewidth = 0.5, linetype = "solid")
             ))
 
+knitr::opts_chunk$set(cache = FALSE, warning = FALSE,
+                      message = FALSE, cache.lazy = FALSE,
+                      fig.height=4, fig.width=6,
+                      fig.asp = 0.618, fig.show = 'show',
+                      out.width = "70%", fig.align = "center")
+
 bed_colnames = c("seqnames", "start", "end", "name", "score", "strand")
 
 #theme_konsta_minimal <- function (base_size = 12)
